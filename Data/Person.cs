1675992c904442ec4caf8a173de29ee7294e33b3
SliesarenkoDima <slesarenkodima1996@gmail.com>
2023-09-16 15:26:22 +0300

    added ef core and azure sql db connection

---
+++ Data/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace learn_azure_app.Data
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ Pages/Persons/Index.cshtml.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using learn_azure_app.Data;
+
+namespace learn_azure_app.Pages_Persons
+{
+    public class IndexModel : PageModel
+    {
+        private readonly learn_azure_app.Data.AppDbContext _context;
+
+        public IndexModel(learn_azure_app.Data.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Person> Person { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Persons != null)
+            {
+                Person = await _context.Persons.ToListAsync();
+            }
+        }
+    }
+}
